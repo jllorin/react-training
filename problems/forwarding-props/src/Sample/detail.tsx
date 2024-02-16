60bf491ae602f847807f7140f1354bf21a2ceec1
Jason Llorin <jllorin@rldatix.com>
2024-02-16 17:42:59 -0500

    forwarding-props

---
+++ problems/forwarding-props/src/Sample/detail.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Footer from './footer';
+function Detail()
+{
+    return <div>
+        Detail
+        <Footer />
+    </div>;
+}
+
+export default Detail;
---
+++ problems/forwarding-props/src/Sample/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from './button';
+function Footer()
+{
+    return <div>
+        Footer
+        <Button />
+    </div>;
+}
+
+export default Footer;
